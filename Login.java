52081818f8af79697fc407b6afcb8cc7bbe8f969
jagadish3497 <44638713+jagadish3497@users.noreply.github.com>
2018-11-14 23:49:27 +0530

    Add files via upload

---
+++ Login.java
@@ -0,0 +1,31 @@
+package dom.jai.ds;
+import java.util.Scanner;
+
+public class Login {
+
+	public static void main(String[] args) {
+		
+		 String username="jai";
+		 String password="1234";
+		 Scanner scanner=new Scanner(System.in);
+		 for(int i=0;i<3;i++)
+		 {
+		 System.out.println("enter username");
+		 username=scanner.next();
+		 System.out.println("enter password");
+		 password=scanner.next();
+		 if(username == username && password == password) {
+			 System.out.println("welcome");
+			 i=3;System.out.println("welcome");
+			 
+			 
+		 }
+		 else if(i==2) {
+			 System.out.println("contact admin");
+			 
+		 }
+		 
+		 
+			}
+			}
+		}
